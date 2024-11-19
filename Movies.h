b0b507db834d2146059ec0415bd57cff407cc45f
Drue Gupta <1031762@my.scccd.edu>
2024-11-19 10:31:48 -0800

    Update Movies.h

--- Movies.h
+++ Movies.h
@@ -13,13 +13,51 @@ private:
 	int ratingCount;
 	double rating;
 public:
+
 	string getTitle(string title)
 	{
-			this->title = title;
+		this->title = title;
 	}
 	void setTitle(string title)
 	{
-
+		this->title;
+	}
+	string getDirector(string director)
+	{
+		this->director = director;
+	}
+	void setDirector(string director)
+	{
+		this->director;
+	}
+	string getGenre(string genre)
+	{
+		this->genre = genre;
+	}
+	void setGenre(string genre)
+	{
+		this->genre;
+	}
+	int getYear(int year)
+	{
+		this->year = year;
+	}
+	void setYear(int year)
+	{
+		this->year;
+	}
+	int getRating(int ratingCount)
+	{
+		this->ratingCount = ratingCount;
+	}
+	void rate(double rating)
+	{
+		this->rating = rating;
+	}
+	void display()
+	{
+		cout << "Movie: " << this->title << ". Director: " << this->director << ". Genre: " << this->genre << ". Year: " 
+			<< this->year << ". Rating: " << this->ratingCount << "out of " << this->rating << "." << endl;
 	}
 };
 #endif
