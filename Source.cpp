b389108f7418da21e64509deea73ddbd2a586d62
Drue Gupta <1031762@my.scccd.edu>
2024-11-19 10:32:51 -0800

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "Movies.h"
+#include "Ratings.h"
+using namespace std;
+
+int main()
+{
+	Movies()
+	Movies("Ambatakum 3: Rescuing Private Thug", "Dreamybull, LoveandLighttv, Astroboy", "Action", 2020, 10, 10);
+}
